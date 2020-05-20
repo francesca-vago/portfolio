6e75af1c78552dd85217f24a556e4cb0e9163815
Francesca Vago <vago.francesca@gmail.com>
2020-05-20 09:58:57 +0100

    Header component

--- src/components/header.js
+++ src/components/header.js
@@ -1,22 +1,14 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import { Navbar } from "./navbar"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
+  <header>
+    <div>
+      {/* <Logo/> */}
+      {/* Navbar */}
+      <h1>
         <Link
           to="/"
           style={{
